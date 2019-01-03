8eca299a5f70df1de759bfb9fc09d531fd01f99e
Trung Tran <chunml.techexplained@gmail.com>
2019-01-03 09:54:40 +0800

    Updates

---
+++ precache-manifest.657b084c9038347af27ef127364a01ac.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "40661b166424bf59ad1f",
+    "url": "/moneyflow/static/css/main.8106cdce.chunk.css"
+  },
+  {
+    "revision": "40661b166424bf59ad1f",
+    "url": "/moneyflow/static/js/main.40661b16.chunk.js"
+  },
+  {
+    "revision": "43f7d69ff5df46b589ac",
+    "url": "/moneyflow/static/css/1.0253d45c.chunk.css"
+  },
+  {
+    "revision": "43f7d69ff5df46b589ac",
+    "url": "/moneyflow/static/js/1.43f7d69f.chunk.js"
+  },
+  {
+    "revision": "5ff108ffc18c67a0ae19",
+    "url": "/moneyflow/static/js/runtime~main.5ff108ff.js"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/moneyflow/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/moneyflow/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/moneyflow/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/moneyflow/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/moneyflow/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/moneyflow/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/moneyflow/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/moneyflow/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/moneyflow/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/moneyflow/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/moneyflow/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/moneyflow/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/moneyflow/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/moneyflow/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/moneyflow/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/moneyflow/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "a8e3c7760b87163b367011274d656801",
+    "url": "/moneyflow/static/media/yutabella.a8e3c776.png"
+  },
+  {
+    "revision": "00c6a20c2d639b5d589d330dea748977",
+    "url": "/moneyflow/index.html"
+  }
+];
